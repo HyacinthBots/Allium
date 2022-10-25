37f0120da32f19eb351fd64059d7be8f82c8df90
NotJansel <73423410+NotJansel@users.noreply.github.com>
2022-10-25 13:35:03 +0200

    make some nice changes to the build command

--- src/main/kotlin/de/notjansel/sbbot/extensions/Build.kt
+++ src/main/kotlin/de/notjansel/sbbot/extensions/Build.kt
@@ -5,9 +5,10 @@ import com.kotlindiscord.kord.extensions.extensions.publicSlashCommand
 import com.kotlindiscord.kord.extensions.types.respond
 import de.notjansel.sbbot.TEST_SERVER_ID
 import de.notjansel.sbbot.utils.*
+import dev.kord.rest.builder.message.create.embed
 
 /**
- * Build Command, to show my monkebrain what build the bot is on.
+ * Build Command, to show my monkebrain what build the bot is on and more
  * @author NotJansel
  * @since 0.1.3
  */
@@ -23,7 +24,29 @@ class Build : Extension() {
             guild(TEST_SERVER_ID)
             action {
                 respond {
-                    content = "The Bot is on build $BUILD"
+                    embed {
+                        title = "General Information"
+                        field {
+                            name = "Dependencies"
+                            inline = false
+                            value = """
+                                KordEx: @kordex@
+                                Gson: @gson@
+                                Jansi: @jansi@
+                                kotlin-logging: @logging@
+                                logback-classic: @logback@
+                                kotlinx-serialization-core: @kx-ser@
+                                groovy: @groovy@
+                                detekt: @detekt@
+                                Kotlin: @kotlin@
+                            """.trimIndent()
+                        }
+                        field {
+                            name = "Build"
+                            inline = false
+                            value = "@version@"
+                        }
+                    }
                 }
             }
         }
