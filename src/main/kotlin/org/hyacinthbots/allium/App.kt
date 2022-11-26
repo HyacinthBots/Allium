720ca7f772ef992a3676316ad8ba856a2c088b57
Jansel <github@notjansel.de>
2022-11-26 19:57:31 +0100

    remove logs entirely
    Signed-off-by: Jansel <github@notjansel.de>

--- src/main/kotlin/org/hyacinthbots/allium/App.kt
+++ src/main/kotlin/org/hyacinthbots/allium/App.kt
@@ -9,11 +9,8 @@ import com.kotlindiscord.kord.extensions.ExtensibleBot
 import com.kotlindiscord.kord.extensions.modules.extra.mappings.extMappings
 import com.kotlindiscord.kord.extensions.utils.env
 import dev.kord.common.entity.Snowflake
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
 import me.shedaniel.linkie.utils.readText
 import org.hyacinthbots.allium.extensions.*
-import java.io.File
 import java.util.*
 
 val TEST_SERVER_ID = Snowflake(
@@ -29,17 +26,6 @@ var splashes = JsonArray()
 private val TOKEN = env("TOKEN")   // Get the bot' token from the env vars or a .env file
 suspend fun main() {
     splashes = JsonParser.parseString({}.javaClass.getResource("/splashes.json")?.readText()).asJsonObject.getAsJsonArray("splashes")
-    if (File("./logs/latest.log").exists()) {
-        File("./logs/latest.log").renameTo(File("./logs/log-${File("./logs/").listFiles()?.size}"))
-        withContext(Dispatchers.IO) {
-            File("./logs/latest.log").createNewFile()
-        }
-    } else {
-        withContext(Dispatchers.IO) {
-            File("./logs/").mkdirs()
-            File("./logs/latest.log").createNewFile()
-        }
-    }
     val bot = ExtensibleBot(TOKEN) {
         extensions {
             add(::EventHooks)
