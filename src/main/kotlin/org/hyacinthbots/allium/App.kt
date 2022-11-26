5aa53badb2d3d04384e15e5612258d25b5e37b8c
Jansel <github@notjansel.de>
2022-11-26 15:04:40 +0100

    just look for the username
    Signed-off-by: Jansel <github@notjansel.de>

--- src/main/kotlin/org/hyacinthbots/allium/App.kt
+++ src/main/kotlin/org/hyacinthbots/allium/App.kt
@@ -3,12 +3,15 @@
  */
 package org.hyacinthbots.allium
 
+import com.google.gson.JsonArray
+import com.google.gson.JsonParser
 import com.kotlindiscord.kord.extensions.ExtensibleBot
 import com.kotlindiscord.kord.extensions.modules.extra.mappings.extMappings
 import com.kotlindiscord.kord.extensions.utils.env
 import dev.kord.common.entity.Snowflake
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import me.shedaniel.linkie.utils.readText
 import org.hyacinthbots.allium.extensions.*
 import java.io.File
 import java.util.*
@@ -21,8 +24,11 @@ val TEST_SERVER_CHANNEL_ID = Snowflake(
     env("TEST_CHANNEL").toLong() // Get the test channel ID from the env vars or a .env file
 )
 
+var splashes = JsonArray()
+
 private val TOKEN = env("TOKEN")   // Get the bot' token from the env vars or a .env file
 suspend fun main() {
+    splashes = JsonParser.parseString({}.javaClass.getResource("/splashes.json")?.readText()).asJsonObject.getAsJsonArray("splashes")
     if (File("./logs/latest.log").exists()) {
         File("./logs/latest.log").renameTo(File("./logs/log-${File("./logs/").listFiles()?.size}"))
         withContext(Dispatchers.IO) {
@@ -36,9 +42,10 @@ suspend fun main() {
     }
     val bot = ExtensibleBot(TOKEN) {
         extensions {
-            add(::Startup)
+            add(::EventHooks)
             add(::Modrinth)
             add(::About)
+            add(::PresenceUpdater)
             extMappings {  }
         }
         i18n {
--- src/main/kotlin/org/hyacinthbots/allium/extensions/EventHooks.kt
+++ src/main/kotlin/org/hyacinthbots/allium/extensions/EventHooks.kt
@@ -3,14 +3,13 @@ package org.hyacinthbots.allium.extensions
 import com.kotlindiscord.kord.extensions.extensions.Extension
 import com.kotlindiscord.kord.extensions.extensions.event
 import dev.kord.common.annotation.KordPreview
-import dev.kord.common.entity.PresenceStatus
 import dev.kord.core.behavior.getChannelOf
 import dev.kord.core.entity.channel.TextChannel
 import dev.kord.core.event.gateway.*
-import dev.kord.core.kordLogger
-import kotlinx.coroutines.flow.count
+import dev.kord.core.event.guild.*
 import org.hyacinthbots.allium.TEST_SERVER_CHANNEL_ID
 import org.hyacinthbots.allium.TEST_SERVER_ID
+import org.hyacinthbots.allium.splashes
 import org.hyacinthbots.allium.utils.*
 
 /**
@@ -20,21 +19,22 @@ import org.hyacinthbots.allium.utils.*
  */
 
 @OptIn(KordPreview::class)
-class Startup : Extension() {
+class EventHooks : Extension() {
     override val name = "startup"
     override suspend fun setup() {
         event<ReadyEvent> {
             action {
                 val onlineLog = kord.getGuildOrNull(TEST_SERVER_ID)?.getChannelOf<TextChannel>(TEST_SERVER_CHANNEL_ID)
                 onlineLog?.createMessage("Bot Online, current version: $BUILD")
+                val entries = splashes.count()
+                val entry = (0 until entries).random()
                 kord.editPresence {
-                    status = PresenceStatus.Online
-                    watching("${kord.guilds.count()} Servers")
+                    playing(splashes.get(entry).asString)
                 }
             }
         }
         event<DisconnectEvent> {
-            action { kordLogger.info("Bot Disconnected.") }
+            action { /**/ }
         }
     }
 }
