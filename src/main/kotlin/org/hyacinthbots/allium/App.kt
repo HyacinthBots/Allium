495d68799df1ef303382508b8947a0c6a18c2005
NotJansel <janselfritz096@gmail.com>
2023-01-20 17:17:09 +0100

    doc-gen works pog

--- src/main/kotlin/org/hyacinthbots/allium/App.kt
+++ src/main/kotlin/org/hyacinthbots/allium/App.kt
@@ -6,8 +6,13 @@ import com.kotlindiscord.kord.extensions.ExtensibleBot
 import com.kotlindiscord.kord.extensions.modules.extra.mappings.extMappings
 import me.shedaniel.linkie.utils.readText
 import org.hyacinthbots.allium.extensions.*
+import org.hyacinthbots.allium.utils.ENVIRONMENT
 import org.hyacinthbots.allium.utils.TOKEN
+import org.hyacinthbots.docgenerator.docsGenerator
+import org.hyacinthbots.docgenerator.enums.CommandTypes
+import org.hyacinthbots.docgenerator.enums.SupportedFileFormat
 import java.util.*
+import kotlin.io.path.Path
 
 var splashes = JsonArray()
 var updatemessages = JsonArray()
@@ -26,6 +31,14 @@ suspend fun main() {
         }
         i18n {
             defaultLocale = Locale.ENGLISH
+        }
+        docsGenerator {
+            enabled = true
+            fileFormat = SupportedFileFormat.MARKDOWN
+            filePath = Path("./docs/commands.md")
+            environment = ENVIRONMENT
+            useBuiltinCommandList = true
+            commandTypes = CommandTypes.ALL
         }
     }
     bot.start()
