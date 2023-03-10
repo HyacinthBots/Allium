3d57fe338ea8c80416a21e8adac96506d82cf08b
NoComment1105 <nocomment1105@outlook.com>
2023-03-10 15:10:08 +0000

    Didnt wanna commit this anyway

---
+++ src/main/kotlin/org/hyacinthbots/allium/database/Database.kt
@@ -0,0 +1,23 @@
+package org.hyacinthbots.allium.database
+
+import com.mongodb.ConnectionString
+import com.mongodb.MongoClientSettings
+import org.bson.UuidRepresentation
+import org.hyacinthbots.allium.utils.MONGO_URI
+import org.litote.kmongo.coroutine.coroutine
+import org.litote.kmongo.reactivestreams.KMongo
+
+class Database {
+    private val settings = MongoClientSettings
+        .builder()
+        .uuidRepresentation(UuidRepresentation.STANDARD)
+        .applyConnectionString(ConnectionString(MONGO_URI))
+        .build()
+
+    private val client = KMongo.createClient(settings).coroutine
+
+    val mongo get() = client.getDatabase("Allium")
+
+    // Soon:tm:
+    // suspend fun migrate() {}
+}
