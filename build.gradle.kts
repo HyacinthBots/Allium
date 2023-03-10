94fc45b4d9225cf717cfba4aee995de38534778d
NotJansel <github.@notjansel.de>
2023-03-10 19:40:14 +0100

    bump version to 0.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,9 +34,9 @@ fun String.runCommand(
 
 
 group = "org.hyacinthbots.allium"
-version = "0.4-build.local-" + "git rev-parse --short=8 HEAD".runCommand(workingDir = rootDir) + "-" + "git branch --show-current".runCommand(workingDir = rootDir).replace("/", ".")
+//version = "0.4-build.local-" + "git rev-parse --short=8 HEAD".runCommand(workingDir = rootDir) + "-" + "git branch --show-current".runCommand(workingDir = rootDir).replace("/", ".")
 var buildTime = Date().time / 1000
-//version = "0.3.3"
+version = "0.4"
 // The current LTS Java version
 val javaVersion = 17
 
