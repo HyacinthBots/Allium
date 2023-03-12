aa8755d29ee62184012fd0c697e882f4f6fdd95a
NotJansel <github@notjansel.de>
2023-03-12 14:03:03 +0100

    set version string to dev one again

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@ plugins {
 
 fun String.runCommand(
     workingDir: File = File("."),
-    timeoutAmount: Long = 59,
+    timeoutAmount: Long = 60,
     timeoutUnit: TimeUnit = TimeUnit.SECONDS
 ): String = ProcessBuilder(split("\\s(?=(?:[^'\"`]*(['\"`])[^'\"`]*\\1)*[^'\"`]*$)".toRegex()))
     .directory(workingDir)
@@ -34,9 +34,9 @@ fun String.runCommand(
 
 
 group = "org.hyacinthbots.allium"
-//version = "0.4-build.local-" + "git rev-parse --short=8 HEAD".runCommand(workingDir = rootDir) + "-" + "git branch --show-current".runCommand(workingDir = rootDir).replace("/", ".")
+version = "0.4.1-build.local-" + "git rev-parse --short=8 HEAD".runCommand(workingDir = rootDir) + "-" + "git branch --show-current".runCommand(workingDir = rootDir).replace("/", ".")
 var buildTime = Date().time / 1000
-version = "0.4"
+//version = "0.4"
 // The current LTS Java version
 val javaVersion = 17
 
