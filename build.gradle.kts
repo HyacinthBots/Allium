5dacf29197ebdf337fbb5ea8ac10c89e6e0121c3
Jannik Reimers <github@notjansel.de>
2024-03-20 00:14:34 +0100

    release 0.6.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,7 +36,7 @@ fun String.runCommand(
 group = "org.hyacinthbots.allium"
 //version = "0.6.0-build.local-" + "git rev-parse --short=8 HEAD".runCommand(workingDir = rootDir) + "-" + "git branch --show-current".runCommand(workingDir = rootDir).replace("/", ".")
 var buildTime = Date().time / 1000
-version = "0.6.1"
+version = "0.6.2"
 // The current LTS Java version
 val javaVersion = 17
 
