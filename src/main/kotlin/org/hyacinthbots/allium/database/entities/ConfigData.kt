de2ad5e28fea7db54944b74e28dda2652f8d8a7a
Jannik Reimers <me@jansel.dev>
2026-05-14 22:00:31 +0200

    make uploading types nullable

--- src/main/kotlin/org/hyacinthbots/allium/database/entities/ConfigData.kt
+++ src/main/kotlin/org/hyacinthbots/allium/database/entities/ConfigData.kt
@@ -6,6 +6,6 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class ConfigData(
 	val guildId: Snowflake,
-	val logUploadingType: String,
-	val linkListenerType: String
+	val logUploadingType: String?,
+	val linkListenerType: String?
 )
